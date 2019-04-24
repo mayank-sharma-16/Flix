628deb1c8ab9d9b66453e0a737ea5386cbd74aae
manksharma <2mayanksharma2@gmail.com>
2019-04-23 23:21:02 -0700

    part 2 finished

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Mayank Sharma on 4/23/19.
+//  Copyright © 2019 Mayank Sharma. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
